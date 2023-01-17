0aeea3a365dd56fc2d2d74e6a9a56c8d4c24433b
NOOBC5 <zhemwlq00101@gmail.com>
2023-01-17 10:11:56 +0900

    기초 작업

---
+++ src/main/java/com/project/exambbs/ExamBbsApplication.java
@@ -0,0 +1,17 @@
+package com.project.exambbs;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+public class ExamBbsApplication {
+	public static void main(String[] args) {
+		SpringApplication.run(ExamBbsApplication.class, args);
+	}
+
+
+}
---
+++ src/test/java/com/project/exambbs/ExamBbsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.project.exambbs;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ExamBbsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
