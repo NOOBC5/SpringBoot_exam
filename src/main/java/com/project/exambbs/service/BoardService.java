48c776bb7e1be20d80368cce411f0390f3363fed
NOOBC5 <zhemwlq00101@gmail.com>
2023-01-19 05:53:43 +0900

    Spring boot 연습 코드

--- src/main/java/com/project/exambbs/service/BoardService.java
+++ src/main/java/com/project/exambbs/service/BoardService.java
@@ -3,9 +3,14 @@ package com.project.exambbs.service;
 import com.project.exambbs.entity.Board;
 import com.project.exambbs.repository.BoardRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
+import org.springframework.web.multipart.MultipartFile;
 
+import java.io.File;
 import java.util.List;
+import java.util.UUID;
 
 @Service
 public class BoardService {
@@ -13,15 +18,28 @@ public class BoardService {
     private BoardRepository boardRepository;
 
     // 글 작성 처리
-    public void write(Board board){
+    public void write(Board board, MultipartFile file) throws Exception{
+
+        String projectPath = System.getProperty("user.dir") + "\\src\\main\\resources\\static\\files";
+
+        UUID uuid = UUID.randomUUID();
+
+        String fileName = uuid + "_" + file.getOriginalFilename();
+
+        File saveFile = new File(projectPath, fileName);
+
+        file.transferTo(saveFile);
+
+        board.setFilename(fileName);
+        board.setFilepath("/files/" + fileName);
 
         boardRepository.save(board);
     }
     
     // 게시글 리스트 처리
-    public List<Board> boardList(){
+    public Page<Board> boardList(Pageable pageable){
 
-        return boardRepository.findAll();
+        return boardRepository.findAll(pageable);
     }
 
 
@@ -29,6 +47,12 @@ public class BoardService {
     public Board boardView(Integer id){
 
         return boardRepository.findById(id).get();
+    }
+
+    // 게시글 검색
+    public Page<Board> boardSearchList(String searchKeyword, Pageable pageable){
+
+        return boardRepository.findByTitleContaining(searchKeyword, pageable);
     }
 
     //특정 게시글 삭제
